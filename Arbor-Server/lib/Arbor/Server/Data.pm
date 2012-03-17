4d51cde7840276684a8f4f1b0d325152a4640766
Dean Pearce <deanpearce@gmail.com>
2012-03-17 12:28:01 -0400

    Add simple pingback service for testing mobile application.

--- Arbor-Server/lib/Arbor/Server/Data.pm
+++ Arbor-Server/lib/Arbor/Server/Data.pm
@@ -1,11 +1,16 @@
 package Arbor::Server::Data;
 use Dancer ':syntax';
 
+set serializer => 'JSON';
+
 prefix '/data';
 
 get '/block/*' => sub {
-   my ( $block_id ) = splat;
-   return "fetching block id $block_id";
+    my ( $block_id ) = splat;
+    return "fetching block id $block_id";
 };
 
+post '/ping' => sub {
+    return { dataMode => params->{mProvider} };
+}
 true;
